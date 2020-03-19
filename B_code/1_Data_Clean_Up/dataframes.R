e651b0756cafed51741abc3f95379a3061798d4b
Bagmon <gra11027@byui.edu>
2020-03-19 21:27:38 +0000

    The code for genre specific data frames was updated so that it contained all movies in the ggplot2movies data set that are in the respective genre and not just those that have budgets listed.

--- B_code/1_Data_Clean_Up/dataframes.R
+++ B_code/1_Data_Clean_Up/dataframes.R
@@ -4,35 +4,28 @@ movies <- ggplot2movies::movies
 
 movies_action <- movies %>% 
   filter(movies$Action == 1) %>% 
-  select(title, year, length, budget, rating, votes) %>% 
-  filter(!is.na(budget))
+  select(title, year, length, budget, rating, votes)
 
 movies_animation <- movies %>% 
   filter(movies$Animation == 1) %>% 
-  select(title, year, length, budget, rating, votes) %>% 
-  filter(!is.na(budget))
+  select(title, year, length, budget, rating, votes) 
 
 movies_comedy <- movies %>% 
   filter(movies$Comedy == 1) %>% 
-  select(title, year, length, budget, rating, votes) %>% 
-  filter(!is.na(budget))
+  select(title, year, length, budget, rating, votes) 
 
 movies_drama <- movies %>% 
   filter(movies$Drama == 1) %>% 
-  select(title, year, length, budget, rating, votes) %>% 
-  filter(!is.na(budget))
+  select(title, year, length, budget, rating, votes)
 
 movies_documentary <- movies %>% 
   filter(movies$Documentary == 1) %>% 
-  select(title, year, length, budget, rating, votes) %>% 
-  filter(!is.na(budget))
+  select(title, year, length, budget, rating, votes) 
 
 movies_romance <- movies %>% 
   filter(movies$Romance == 1) %>% 
-  select(title, year, length, budget, rating, votes) %>% 
-  filter(!is.na(budget))
+  select(title, year, length, budget, rating, votes) 
 
 movies_short <- movies %>% 
   filter(movies$Short == 1) %>% 
-  select(title, year, length, budget, rating, votes) %>% 
-  filter(!is.na(budget))
+  select(title, year, length, budget, rating, votes)
